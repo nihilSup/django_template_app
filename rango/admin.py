ab46a497ba06ffb7ee148e7ad09aacd058472c66
Stulikov Igor Sergeevich <IStulikov@gksm.local>
2017-12-26 20:25:16 +0300

    added tests, admin page tweaks, models some new fields

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+admin.site.register(Category)
+
+class PageAdmin(admin.ModelAdmin):
+    'custom functionality for '
+    list_display = ('title', 'category', 'url')
+    fieldsets = [
+        ('Main info:', {'fields': ['category']}),
+        ('Details:', {'fields': ['title', 'url', 'views']})
+    ]
+admin.site.register(Page, PageAdmin)
